6058e859c6c33dca93804017a4a97f43a80fd657
boaltamirano <brayanaltamiranoperez@gmail.com>
2024-02-17 23:52:50 -0500

    fix: event feed

---
+++ feedService/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go/events-cqrs-go/database"
+	"github.com/go/events-cqrs-go/events"
+	"github.com/go/events-cqrs-go/repository"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	PostgresDB       string `envconfig:"POSTGRES_DB"`
+	PostgresUser     string `envconfig:"POSTGRES_USER"`
+	PostgresPassword string `envconfig:"POSTGRES_PASSWORD"`
+	NatsAddress      string `envconfig:"NATS_ADDRESS"`
+}
+
+func main() {
+	var cfg Config
+
+	err := envconfig.Process("", &cfg)
+
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	addr := fmt.Sprintf("postgres://%s:%s@postgres/%s?sslmode=disable", cfg.PostgresUser, cfg.PostgresPassword, cfg.PostgresDB)
+
+	repo, err := database.NewPostgresRepository(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository.SetRepository(repo)
+
+	n, err := events.NewNats(fmt.Sprintf("nats://%s", cfg.NatsAddress))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	events.SetEventStore(n)
+
+	defer events.Close()
+
+	router := newRouter()
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
